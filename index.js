ff39b3c8c4357a8e933267e6e8a96d6d6b8289b3
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-07-11 00:30:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ app.use(express.json());
 
 app.use("/recipes", recipeRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000");
 });
