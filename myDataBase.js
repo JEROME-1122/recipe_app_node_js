691f1b758192e678c1973222cce51b1b866f163f
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-07-11 00:33:50 +0530

    Update myDataBase.js

--- myDataBase.js
+++ myDataBase.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.DB);
+    await mongoose.connect("mongodb+srv://jeromfranklin160:TZP2q2VDACsB3b1O@cluster0.05rxsif.mongodb.net/recipeDB?retryWrites=true&w=majority&appName=Cluster0);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("DB connection error:", error);
